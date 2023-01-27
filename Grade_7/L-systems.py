163f19d19a05598382d652487a25f3ab03e5d377
BasilNozdrin <bassvass@gmail.com>
2023-01-27 16:24:27 +0300

    Загружено задание на 27/01

--- Grade_7/L-systems.py
+++ Grade_7/L-systems.py
@@ -1,4 +1,5 @@
 import turtle
+import numpy as np
 
 turtle.tracer(1, 0)
 turtle.ht()
@@ -14,7 +15,8 @@ def make_string(axiom, *rules, iterations=5):
     return result
 
 
-def draw_string(string, step_size=10, angle=90):
+def draw_string(string, step_size=10, angle=90, colors=None):
+    counter = 0
     for c in string:
         if c == 'F':
             turtle.fd(step_size)
@@ -26,15 +28,23 @@ def draw_string(string, step_size=10, angle=90):
             turtle.pu()
             turtle.fd(step_size)
             turtle.pd()
+        if c == 'C':
+            if colors is None:
+                continue
+            turtle.color(colors[counter % len(colors)])
+            counter += 1
 
 
 def koch(iterations):
-    turtle.width(2)
+    turtle.width(5)
     turtle.pu()
     turtle.setpos(-400, -200)
     turtle.pd()
     koch_str = make_string('F', ('F', 'FLFRRFLF'), iterations=iterations)
-    draw_string(koch_str, step_size=5, angle=60)
+    koch_str = koch_str.replace('F', 'CF')
+    colors_len = koch_str.count('C')
+    colors = list(map(lambda row: tuple(row), np.linspace([0, 1, 0], [1, 1, 0], num=colors_len)))
+    draw_string(koch_str, step_size=5, angle=60, colors=colors)
 
 
 def dragon(iterations):
@@ -53,9 +63,9 @@ def serp(iterations):
     draw_string(serp_str, step_size=10, angle=120)
 
 
-# koch(5)
+koch(5)
 # dragon(3)
-serp(5)
+# serp(5)
 turtle.done()
 
 # print(len(make_string('F', ('F', 'FLFRRFLF'), iterations=5)))
