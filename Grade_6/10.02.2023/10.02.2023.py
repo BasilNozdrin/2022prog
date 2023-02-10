bc983e2319cbe44136e58a975187c8e56f985730
BasilNozdrin <bassvass@gmail.com>
2023-02-10 12:49:31 +0300

    10/02/2023 upd

--- Grade_6/10.02.2023/10.02.2023.py
+++ Grade_6/10.02.2023/10.02.2023.py
@@ -5,6 +5,10 @@ print(a.some_function(5))
 print(some_function(12))
 
 # напишите файл Player.py
+# таким образом, чтобы можно было использовать его
+# в других программах
+
+# например, чтобы можно было написать следующую программу:
 
 # import Player
 #
@@ -12,3 +16,6 @@ print(some_function(12))
 # player2 = Player.create_player(...)
 # player1, player2 = Player.attack(player1, player2)
 # Player.attack(player2, player1)
+
+# Домашка состоит из двух файлов - Player.py и еще одного
+# присылайте оба файла
