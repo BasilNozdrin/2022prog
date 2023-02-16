1f759952284302411131810d32bc21a4077ba0d3
BasilNozdrin <bassvass@gmail.com>
2023-02-16 16:37:44 +0300

    7bio 16.02.2023

---
+++ Grade_7/plot.py
@@ -0,0 +1,20 @@
+# Import Meteostat library and dependencies
+from datetime import datetime
+import matplotlib.pyplot as plt
+from meteostat import Point, Daily
+
+# Set time period
+start = datetime(2018, 1, 1)
+end = datetime(2018, 12, 31)
+
+# Create Point for Vancouver, BC
+location = Point(49.2497, -123.1193, 70)
+
+# Get daily data for 2018
+data = Daily(location, start, end)
+data = data.fetch()
+
+# Plot line chart including average, minimum and maximum temperature
+ax = data.plot(y=['tavg'])
+ax.set(title='title asdasd')
+plt.show()
